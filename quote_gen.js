53ad136677e16bddd62aadbcb33043b352827d7d
NBooseCodes <nmboose@gmail.com>
2024-08-05 11:41:24 -0400

    test

--- quote_gen.js
+++ quote_gen.js
@@ -94,6 +94,7 @@ app.post("/data", function(req, res) {
     let data = req.body
     let arr = Object.keys(data)[0]
     console.log(arr)
+    JSON.parse(arr)
     console.log(data)
     console.log(data["test"])
 
