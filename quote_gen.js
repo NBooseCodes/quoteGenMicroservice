4c2ce50d9f280079d57f8adc1e026fea600d48a2
NBooseCodes <nmboose@gmail.com>
2024-08-05 11:21:22 -0400

    test

--- quote_gen.js
+++ quote_gen.js
@@ -92,7 +92,7 @@ app.post("/data", function(req, res) {
     
     //console.log(req)
     let data = req.body
-    console.log(data[0][0])
+    console.log(data[0])
     console.log(req)
     console.log(req.body)
     res.send(JSON.stringify({thng: "hello"}))
