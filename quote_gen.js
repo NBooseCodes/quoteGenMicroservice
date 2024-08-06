0cecae7769ad1e28496def2235e05f54af79806e
NBooseCodes <nmboose@gmail.com>
2024-08-06 11:56:32 -0400

    test

--- quote_gen.js
+++ quote_gen.js
@@ -74,28 +74,8 @@ function quoteSelector (type) {
             quote: 'There\'s no such thing as bad weather, just soft people.'
         }
     }
-    /*let weatherQuotesDict = 
-    {
-        "Wherever you go, no matter what the weather, always bring your own sunshine.":"Anthony J. D'Angelo",
-        "There's no such thing as bad weather, just soft people.":"Bill Bowerman",
-        "The storm starts, when the drops start dropping. When the drops stop dropping then the storm starts stopping.":"Dr. Seuss",
-        "A lot of people like snow. I find it to be an unnecessary freezing of water.":"Carl Reiner",
-        "Summer in the deep South is not only a season, a climate, it's a dimension. Floating in it, one must be either proud or submerged.":"Eugene Walter"
-    };
-
-   /* let businessQuoteDict = 
-    {
-        "There are no secrets to success. It is the result of preparation, hard work, and learning from failure.":"Colin Powell",
-        "A brand for a company is like a reputation for a person. You earn reputation by trying to do hard things well.":"Jeff Bezos",
-        "Details create the big picture.":"Sanford I. Well",
-        "Our work is the presentation of our capabilities.":"Edward Gibbon",
-        "The first responsibility of a leader is to define reality. The last is to say thank you. In between, the leader is a servant.":"Max de Pree"
-    };*/
-
-
 
     let randomNum = getRandomInt(3);
-    console.log(randomNum)
 
     if (type === "business") {
         return bizQuote[randomNum]
@@ -124,9 +104,7 @@ app.post("/data", function(req, res) {
     //console.log(req)
     let data = req.body
     let arr = Object.keys(data)[0]
-    console.log(arr)
     parsedJSON = JSON.parse(arr)
-    console.log(parsedJSON["type"])
     let quoteData = quoteSelector(parsedJSON["type"])
     res.send(JSON.stringify(quoteData))
 
